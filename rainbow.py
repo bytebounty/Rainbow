76374b8e2db1db4ae211a9b9b3a09b0e298041ed
Nebula <43358273+nebula-cpp@users.noreply.github.com>
2020-02-27 12:29:00 -0800

    Update rainbow.py

--- rainbow.py
+++ rainbow.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import hashlib
+import sys
 import os
 import os.path
 from os import path
 import glob
+
+##########################################
+#Enable ONLY if RAM is fast and abundant.
+RAM = False
+##########################################
+
+DIR = os.getcwd()
 
 def isFile(path):
 	if os.path.isdir(path):
@@ -13,8 +21,50 @@ def isFile(path):
 	else:
 		return 3
 
-def hash(string):
-	return hashlib.md5(string.strip()).hexdigest()
+usage = """
+USAGE: """+ sys.argv[0] +""" [HASH TYPE]
+
+OPTIONS:
+ SHA1,
+ SHA224,
+ SHA256,
+ SHA384,
+ SHA512,
+ MD5
+"""
+
+if len(sys.argv) < 2:
+	print(usage)
+	exit()
+
+################
+
+hashType = sys.argv[1].lower()
+
+################
+
+if hashType == "md5":
+	def hash(string):
+		return hashlib.md5(string.strip()).hexdigest()
+elif hashType == "sha1":
+	def hash(string):
+		return hashlib.sha1(string.strip()).hexdigest()
+elif hashType == "sha224":
+	def hash(string):
+		return hashlib.sha224(string.strip()).hexdigest()
+elif hashType == "sha256":
+	def hash(string):
+		return hashlib.sha256(string.strip()).hexdigest()
+elif hashType == "sha384":
+	def hash(string):
+		return hashlib.sha384(string.strip()).hexdigest()
+elif hashType == "sha512":
+	def hash(string):
+		return hashlib.sha512(string.strip()).hexdigest()
+else:
+	print(usage)
+	exit()
+
 
 banner = """
 ▄▄▄   ▄▄▄· ▪    ▐ ▄  ▄▄▄▄·	 ▄▄▌ ▐  ▄▌
@@ -25,13 +75,14 @@ banner = """
 """
 
 print(banner)
+print("Hash type: " + hashType.upper() + "\n")
 
 while True:
 	print(""" [1] Generate rainbow table
 
  [2] Unhash with rainbow table
 
- [3] Crack hashes without rainbow table
+ [3] Unhash without rainbow table
 
  [4] Generate hash based on key
 """)
@@ -39,121 +90,143 @@ while True:
 	if Input == "1":
 		filename = raw_input("Name of file for rainbow table: ")
 		wordlist = raw_input("Path to wordlist file or folder: ")
-		
+		print("")
 		if isFile(wordlist) == False:
 			os.chdir(wordlist)
+			wordlist = open(filename, 'a')
 			for file in glob.glob("*.txt"):
-				#print(file)
-				tmp = open("tmp", 'a')
-				readFile = open(file, 'r')
-				readFileLines = readFile.readlines()
-				for line in readFileLines:
-					tmp.writelines(line)
-			tmp.close()
-			wordlist = "tmp"
-
-		readFile = open(wordlist, 'r')
-		readFileLines = readFile.readlines()
-
-		wordlist = open(filename, 'a')
-		for line in readFileLines:
-			wordlist.writelines(hash(line) + "|" + line)
-			line = (line[0].swapcase() + line[1:])
-			wordlist.writelines(hash(line) + "|" + line)
+				if RAM:
+					readFile = open(file, 'r')
+					readFileLines = readFile.readlines()
+				with open(file) as fileobject:
+					if RAM == False:
+						readFileLines = fileobject
+					for line in readFileLines:
+						wordlist.writelines(hash(line) + "|" + line)
+						if line[0].isalpha():
+							line = (line[0].swapcase() + line[1:])
+							wordlist.writelines(hash(line) + "|" + line)
 
 		wordlist.close()
-		
-		if path.exists("tmp"):
-			try:
-				os.remove("tmp")
-			except:
-				print("Failed to remove tmp file.")
 	elif Input == "2":
 		rainbow = raw_input("Name of file for rainbow table: ")
 		if rainbow == "":
 			rainbow = "rainbow"
 		HASH = raw_input("Hash (or file): ")
 
-		print("Loading rainbow table...")
-		readFile = open(rainbow, 'r')
-		readFileLines = readFile.readlines()
-		print("Table loaded.\n")
+		if RAM:
+			print("Loading rainbow table...")
+			readFile = open(rainbow, 'r')
+			readFileLines = readFile.readlines()
+			print("Table loaded.\n")
+
+		times = 0
 
 		if isFile(HASH) and path.exists(HASH):
-			print("Loading hashes...")
-			readHash = open(HASH, 'r')
-			readHashLines = readHash.readlines()
-			print("Hashes loaded.\n")
-			for line in readHashLines:
-				print("Hash: " + line.strip())
-				found = False
-				for hashline in readFileLines:
-					#print(hashline),
-					if line.strip() in hashline:
-						if line.strip() == hashline.split("|")[0]:
-							found = True
-							print("KEY FOUND: " + hashline.split("|")[1])
-							break
-				if found == False:
-					print("[!] KEY NOT FOUND [!]\n")
+			if RAM:
+				print("Loading hashes...")
+				readHash = open(HASH, 'r')
+				readHashLines = readHash.readlines()
+				print("Hashes loaded.\n")
+			with open(HASH) as fileobject:
+				if RAM == False:
+					readHashLines = fileobject
+					for line in readHashLines:
+						print("Hash: " + line.strip())
+						found = False
+						with open(rainbow) as fileobject:
+							if RAM == False:
+								readFileLines = fileobject
+							for hashline in readFileLines:
+								times += 1
+								#print(hashline),
+								if line.strip() in hashline:
+									if line.strip() == hashline.split("|")[0]:
+										found = True
+										print("KEY FOUND: " + hashline.split("|")[1])
+										break
+						if found == False:
+							print("[!] KEY NOT FOUND [!]\n")
+				print("Compared " + str(times) + " hashes.\n")
 		
 		else:
+			
+			###readFileLines
 			found = False
-			for line in readFileLines:
-				#print(line),
-				if HASH in line:
-					if HASH == line.split("|")[0]:
-						found = True
-						print("KEY FOUND: " + line.split("|")[1])
-						break
+			with open(rainbow) as fileobject:
+				if RAM == False:
+					readFileLines = fileobject
+				for line in readFileLines:
+					times += 1
+					#print(line),
+					if HASH in line:
+						if HASH == line.split("|")[0]:
+							found = True
+							print("KEY FOUND: " + line.split("|")[1])
+							break
 			if found == False:
 				print("[!] KEY NOT FOUND [!]\n")
+			print("Compared " + str(times) + " hashes.\n")
 				
 				
 	elif Input == "3":
 		wordlist = raw_input("Path to wordlist file or folder: ")
 		HASH = raw_input("Hash (or file): ")
+		if RAM:
+			print("Loading wordlist...")
+			readFile = open(wordlist, 'r')
+			readFileLines = readFile.readlines()
+			print("Wordlist loaded.\n")
 		if isFile(wordlist) == False:
 			os.chdir(wordlist)
 			for file in glob.glob("*.txt"):
-				#print(file)
-				tmp = open("tmp", 'a')
-				readFile = open(file, 'r')
-				readFileLines = readFile.readlines()
-				for line in readFileLines:
-					tmp.writelines(line)
-			tmp.close()
-			wordlist = "tmp"
-		print("Loading wordlist...")
-		readFile = open(wordlist, 'r')
-		readFileLines = readFile.readlines()
-		print("Wordlist loaded.\n")
-		
-		if isFile(HASH) and path.exists(HASH):
-			found = False
-			
-			print("Loading hashes...")
-			hashFile = open(HASH, 'r')
-			readHashLines = hashFile.readlines()
-			print("Hashes loaded.\n")
-			
-			for hashes in readHashLines:
-				for lines in readFileLines:
-					if hash(lines) == hashes.strip():
-						print("KEY FOUND: " + lines)
-						found = True
+				if RAM:
+					readFile = open(file, 'r')
+					readFileLines = readFile.readlines()
+				with open(file) as fileobject:
+					if RAM == False:
+						readFileLines = fileobject
+					for line in readFileLines:
+						if isFile(HASH) and path.exists(HASH):
+							found = False
+							if RAM:
+								print("Loading hashes...")
+								hashFile = open(HASH, 'r')
+								readHashLines = hashFile.readlines()
+								print("Hashes loaded.\n")
+							with open(HASH) as fileobject:
+								if RAM == False:
+									readHashLines = fileobject
+								for hashes in readHashLines:
+									for line in readFileLines:
+										if hash(line) == hashes.strip():
+											print("KEY FOUND: " + line)
+											found = True
+											break
+									if found == False:
+										print("[!] KEY NOT FOUND [!]\n")
+						else:
+							found = False
+							if hash(line) == HASH:
+								print("KEY FOUND: " + line)
+								found = True
+								break
+					if found == False:
+						print("[!] KEY NOT FOUND [!]\n")
 						break
-				if found == False:
-					print("[!] KEY NOT FOUND [!]")
-		else:
-			found = False
-			for lines in readFileLines:
-				if hash(lines) == HASH:
-					print("KEY FOUND: " + lines)
-					found = True
-					break
-			if found == False:
-				print("[!] KEY NOT FOUND [!]")
+					else:
+						break
+		
+		
 
 	elif Input == "4":
 		print("Hash: " + hash( raw_input("Key: ") ) )
+		print("")
+	else:
+		print("\n Invalid option\n")
+	if path.exists("tmp"):
+		try:
+			os.remove("tmp")
+		except:
+			print("Failed to remove any temporary files.\n")
+		os.chdir(DIR)
